7902d9c7afb9dedf90d441f0454bd513901f7846
pri-sharm <112990023+pri-sharm@users.noreply.github.com>
2023-02-07 13:01:39 -0800

    Update WeatherBot.java

--- src/WeatherBot.java
+++ src/WeatherBot.java
@@ -1,12 +1,38 @@
 import core.data.*;
 
 public class WeatherBot {
-   
+	 public static void main(String[] args) {
+		   
+		   WeatherBot bot = new WeatherBot("KSEA");
+		   WeatherBot bot2 = new WeatherBot("KRNT");
+		   Observation ob1 = bot.getObservation();
+		   Observation ob2 = bot2.getObservation();
+		   System.out.println(ob1.toString());
+		   System.out.println(ob2.toString());
+		   
+		   WeatherBot bot3 = new WeatherBot("KSMP");
+		   Observation ob3 = bot2.getObservation();
+		   System.out.println(ob3.toString());
+		   
+		   if (ob2.colderThan(ob3)) {
+			   System.out.println("It is colder at " +ob2.getId() + " than "  + ob3.getId());
+		   }
+		   else {
+			   System.out.println("It is warmer at " +ob2.getId() + " than "  + ob3.getId());
+		   }
+		   
+		   System.out.println(ob2.getId() +": " + ob2.getWindConditions());
+		   System.out.println(ob3.getId() +": " + ob3.getWindConditions());
+	   }   
+
+
+	private String stationID;
    /**
     * Constructs a WeatherBot object with the specified weather station id
     * @param stationID the weather station id
     */
    public WeatherBot(String stationID) {
+	   this.stationID = stationID;
    }
    
    
@@ -16,35 +42,16 @@ public class WeatherBot {
     * @return an Observation object
     */
    public Observation getObservation() {
-	   return null;
+	   Activity1.avoidSSLError();
+		String id = this.stationID;
+		DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/" + id + ".xml"); 
+		ds.setCacheTimeout(15);  
+		ds.load();
+		ds.printUsageString();
+	   Observation ob = ds.fetch("Observation", "station_id", "weather", "temp_f", "wind_degrees",  "wind_kt", "pressure_mb", "relative_humidity");
+	   return ob;
    }
    
-   public static void main(String[] args) {
-	   
-	   WeatherBot bot = new WeatherBot("KSEA");
-	   WeatherBot bot2 = new WeatherBot("KRNT");
-	   Observation ob1 = bot.getObservation();
-	   Observation ob2 = bot2.getObservation();
-	   System.out.println(ob1.toString());
-	   System.out.println(ob2.toString());
-	   
-	   WeatherBot bot3 = new WeatherBot("KSMP");
-	   Observation ob3 = bot2.getObservation();
-	   System.out.println(ob3.toString());
-	   
-	   if (ob2.colderThan(ob3)) {
-		   System.out.println("It is colder at " +ob2.getId() + " than "  + ob3.getId());
-	   }
-	   else {
-		   System.out.println("It is warmer at " +ob2.getId() + " than "  + ob3.getId());
-	   }
-	   
-	   System.out.println(ob2.getId() +": " + ob2.getWindConditions());
-	   System.out.println(ob3.getId() +": " + ob3.getWindConditions());
-   }   
-
+  
 
 }
-
-
-
